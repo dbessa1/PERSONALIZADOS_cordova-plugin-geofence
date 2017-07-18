22a41bbbab579ad7b5b317d2563650bc210d53df
dbessa1 <daniel.de.bessa@gmail.com>
2017-07-18 00:46:08 +0000

    Update TransitionReceiver.java

--- src/android/TransitionReceiver.java
+++ src/android/TransitionReceiver.java
@@ -1,19 +1,2 @@
-import com.cowbell.cordova.geofence.Gson;
-import com.cowbell.cordova.geofence.GeoNotification;
-
-public class TransitionReceiver extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        String error = intent.getStringExtra("error");
-
-        if (error != null) {
-            //handle error
-            Log.println(Log.ERROR, "YourAppTAG", error);
-        } else {
-            String geofencesJson = intent.getStringExtra("transitionData");
-            GeoNotification[] geoNotifications = Gson.get().fromJson(geofencesJson, GeoNotification[].class);
-            //handle geoNotifications objects
-        }
-    }
-}
+Intent launchIntent = getActivity.getPackageManager().getLaunchIntentForPackage("com.grantec.filhorapido");
+        startActivity(launchIntent);
